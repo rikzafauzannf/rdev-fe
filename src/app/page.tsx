7c4cdd4c4f5c69a0a17a58a9c0076c79c53e5849
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2023-10-11 12:46:05 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,30 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-items-center align-middle items-center w-full h-screen">
+      <div className="grid justify-items-center align-middle items-center w-full h-screen">
         <>
-        <Image
-          src={"/me.JPG"}
-          alt={"me"}
-          width={300}
-          height={300}
-          className="mask mask-parallelogram-4 w-1/4"
-        />
-        <h1 className="text-2xl">Rikza Fauzan Nurfadilah</h1>
-        <h1 className="text-4xl font-bold">
-          <Typewriter
-            options={{
-              strings: ["Front-End Dev", "NextJs", "TailwindCSS", "Bootstrap"],
-              autoStart: true,
-              loop: true,
-            }}
+          <Image
+            src={"/me.JPG"}
+            alt={"me"}
+            width={300}
+            height={300}
+            className="mask mask-parallelogram-4 w-1/4"
           />
-        </h1>
+          <h1 className="text-2xl">Rikza Fauzan Nurfadilah</h1>
+          <h1 className="text-4xl font-bold">
+            <Typewriter
+              options={{
+                strings: [
+                  "Front-End Dev",
+                  "NextJs",
+                  "TailwindCSS",
+                  "Bootstrap",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
         </>
       </div>
     </>
