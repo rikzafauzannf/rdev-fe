12ff6bb7518c62fb5f8eb800f9685f3e85fd6a90
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2023-10-11 12:54:06 +0700

    positionSetting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 import Typewriter from "typewriter-effect";
-import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <div className="grid justify-items-center align-middle items-center w-full h-screen">
-        <>
-          <Image
-            src={"/me.JPG"}
-            alt={"me"}
-            width={300}
-            height={300}
-            className="mask mask-parallelogram-4 w-1/4"
-          />
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center align-middle items-center w-full h-screen">
+        <img src={"/me.JPG"} className="mask mask-parallelogram-4" />
+        <div>
           <h1 className="text-2xl">Rikza Fauzan Nurfadilah</h1>
           <h1 className="text-4xl font-bold">
             <Typewriter
@@ -28,7 +22,7 @@ export default function Home() {
               }}
             />
           </h1>
-        </>
+        </div>
       </div>
     </>
   );
